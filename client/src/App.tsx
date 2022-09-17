a395d574eefc1a587be6ace0b45a4da4538063bd
mhshifat <mehedihassanshifat01@gmail.com>
2022-09-17 14:40:51 +0600

    added auth context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,26 +5,33 @@ import ConversationPage from "./pages/ConversationPage";
 import ConversationChannelPage from "./pages/ConversationChannelPage";
 import ConversationPanel from "./components/conversations/ConversationPanel";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import { AuthContext } from "./utils/contexts/AuthContext";
+import { useState } from "react";
+import { User } from "./utils/types";
 
 function App() {
+	const [user, setUser] = useState<User | undefined>(undefined);
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route
-					path="conversations"
-					element={
-						<AuthenticatedRoute>
-							<ConversationPage />
-						</AuthenticatedRoute>
-					}
-				>
-					<Route index element={<ConversationPanel />} />
-					<Route path=":id" element={<ConversationChannelPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<AuthContext.Provider value={{ user, updateAuthUser: setUser }}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route
+						path="conversations"
+						element={
+							<AuthenticatedRoute>
+								<ConversationPage />
+							</AuthenticatedRoute>
+						}
+					>
+						<Route index element={<ConversationPanel />} />
+						<Route path=":id" element={<ConversationChannelPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</AuthContext.Provider>
 	);
 }
 
--- client/src/hooks/useAuth.tsx
+++ client/src/hooks/useAuth.tsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from "react";
-import { User } from "../utils/types";
+import { useContext, useEffect, useRef, useState } from "react";
+import { AuthContext } from "../utils/contexts/AuthContext";
 import { getLoggedInUser } from "./../utils/api";
 
 export default function useAuth() {
 	const [loading, setLoading] = useState(true);
-	const [user, setUser] = useState<User | null>(null);
+	const { user, updateAuthUser } = useContext(AuthContext);
+	const updateAuthUserRef = useRef(updateAuthUser);
 
 	useEffect(() => {
 		const controller = new AbortController();
 
 		getLoggedInUser(controller)
-			.then(({ data }) => setUser(data))
+			.then(({ data }) => updateAuthUserRef?.current?.(data))
 			.catch(console.error)
 			.finally(() => setTimeout(() => setLoading(false), 1000));
 
