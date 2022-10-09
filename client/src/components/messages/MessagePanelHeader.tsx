f7bb32e5d941e502fc7877009afb127232900689
shifat <shifat@shifat.localdomain>
2022-10-09 14:14:02 +0600

    updated conversation header dynamicly

--- client/src/components/messages/MessagePanelHeader.tsx
+++ client/src/components/messages/MessagePanelHeader.tsx
@@ -1,7 +1,19 @@
+import { useContext, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { RootState } from "../../store";
+import { AuthContext } from "../../utils/contexts/AuthContext";
 import { MessagePanelHeaderStyle } from "../../utils/styles";
 
 export default function MessagePanelHeader() {
-	return (
-		<MessagePanelHeaderStyle>Mehedi Hassan Shifat</MessagePanelHeaderStyle>
+  const { id } = useParams();
+  const { user } = useContext(AuthContext);
+  const conversation = useSelector((state: RootState) => state.conversationsState.conversations.find(con => con.id === +id!));
+
+  const conversationRecipient = useMemo(() => {
+    return user?.id === conversation?.creator.id ? conversation?.recipient : conversation?.creator;
+  }, [user, conversation])
+  return (
+		<MessagePanelHeaderStyle>{conversationRecipient?.firstName} {conversationRecipient?.lastName}</MessagePanelHeaderStyle>
 	);
 }
